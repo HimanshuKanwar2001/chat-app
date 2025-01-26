016e304fddea3ad89695c609cf5b481641b24394
himanshukanwar2001 <himanshukanwar2001@gmail.com>
2025-01-27 02:43:00 +0530

    chat-application

---
+++ mern-chat-app/backend/db/connectToMongoDB.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config(); // Load environment variables
+
+const connectToMongoDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_DB_URI);
+    console.log(`Connected to MongoDB`);
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error.message);
+  }
+};
+
+export default connectToMongoDB;
